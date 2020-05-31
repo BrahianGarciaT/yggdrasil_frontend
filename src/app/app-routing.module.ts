5db4a3731926e9007bf56fa8291ff8b8186fcf40
Brahian Stiven Garcia Torres <brahian7410@gmail.com>
2020-05-31 14:59:42 -0500

    Creacion de rutas basicas del nav y comienzo del modulo de registrar usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { PartidasComponent } from './components/partidas/partidas.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: InicioComponent },
+  { path: 'eventos', component: EventosComponent },
+  { path: 'partidas', component: PartidasComponent },
+  { path: 'registrar', component: RegistrarComponent },
+  { path: 'ingresar', component: IngresarComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { IngresarComponent } from './components/ingresar/ingresar.component';
+import { RegistrarComponent } from './components/registrar/registrar.component';
+import { PartidasComponent } from './components/partidas/partidas.component';
+import { EventosComponent } from './components/eventos/eventos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    InicioComponent,
+    IngresarComponent,
+    RegistrarComponent,
+    PartidasComponent,
+    EventosComponent
   ],
   imports: [
     BrowserModule,
